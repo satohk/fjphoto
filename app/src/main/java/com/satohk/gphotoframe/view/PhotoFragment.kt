7ad00cfd88006b0056a2e5e1b873304a6bfad2bd
satohk <satohk@gmail.com>
2022-11-24 23:49:30 +0900

    impl. videoView cross-fade, videoView error toast

--- app/src/main/java/com/satohk/gphotoframe/view/PhotoFragment.kt
+++ app/src/main/java/com/satohk/gphotoframe/view/PhotoFragment.kt
@@ -1,19 +1,18 @@
 package com.satohk.gphotoframe.view
 
 import android.animation.ObjectAnimator
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.LoadControl
-import com.google.android.exoplayer2.MediaItem
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.trackselection.TrackSelector
 import com.google.android.exoplayer2.ui.StyledPlayerView
@@ -22,9 +21,8 @@ import com.satohk.gphotoframe.R
 import com.satohk.gphotoframe.viewmodel.GridContents
 import com.satohk.gphotoframe.viewmodel.PhotoViewModel
 import kotlinx.android.synthetic.main.fragment_photo.*
-import kotlinx.android.synthetic.main.fragment_photo.view.*
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.time.format.DateTimeFormatter
 
@@ -34,10 +32,10 @@ import java.time.format.DateTimeFormatter
  */
 class PhotoFragment() : Fragment(R.layout.fragment_photo) {
     private val _viewModel by viewModel<PhotoViewModel>()
-    private lateinit var _showImageView: ImageView
-    private lateinit var _hideImageView: ImageView
-    private lateinit var _videoView: StyledPlayerView
-    private var _player: ExoPlayer? = null
+    private val _imageViews = mutableListOf<ImageView>()
+    private val _videoViews = mutableListOf<StyledPlayerView>()
+    private val _videoPlayers = mutableListOf<ExoPlayer>()
+    private var _currentMediaView: View? = null
     private var _slideshowMode: Boolean = true
     private var _startIndex: Int = 0
     private var _contents: GridContents? = null  // If null, use the value set in Setting
@@ -55,20 +53,36 @@ class PhotoFragment() : Fragment(R.layout.fragment_photo) {
         Log.d("PhotoFragment", _startIndex.toString())
         Log.d("PhotoFragment", _contents.toString())
 
-        _showImageView = view.findViewById(R.id.imageView1)
-        _hideImageView = view.findViewById(R.id.imageView2)
-        _videoView = view.findViewById(R.id.videoView)
-        _videoView.controllerAutoShow = false
-
-        _hideImageView.alpha = 0.0f
-        _showImageView.setImageResource(R.drawable.blank_image)
+        _imageViews.add(view.findViewById(R.id.imageView1))
+        _imageViews.add(view.findViewById(R.id.imageView2))
+        _videoViews.add(view.findViewById(R.id.videoView1))
+        _videoViews.add(view.findViewById(R.id.videoView2))
+        for(imageView in _imageViews){
+            imageView.setImageResource(R.drawable.blank_image)
+            imageView.visibility = View.INVISIBLE
+            imageView.alpha = 0.0f
+        }
+        for(videoView in _videoViews) {
+            videoView.controllerAutoShow = false
+            videoView.visibility = View.INVISIBLE
+            videoView.alpha = 0.0f
+        }
 
         // focusをコントロールするためのダミーボタン
         val dummyButton = view.findViewById<Button>(R.id.dummyButtonTop)
         dummyButton.setOnKeyListener { _: View, _: Int, keyEvent: KeyEvent ->
             Log.d("PhotoFragment", "KeyEvent " + keyEvent.toString())
             if(keyEvent.action == KeyEvent.ACTION_DOWN) {
-                if(_videoView.visibility != View.VISIBLE) {
+                if(_currentMediaView is StyledPlayerView){
+                    when (keyEvent.keyCode) {
+                        KeyEvent.KEYCODE_DPAD_DOWN -> (_currentMediaView as StyledPlayerView).showController()
+                        KeyEvent.KEYCODE_DPAD_LEFT -> _viewModel.goPrev()
+                        KeyEvent.KEYCODE_DPAD_RIGHT -> _viewModel.goNext()
+                        else -> return@setOnKeyListener false
+                    }
+                    return@setOnKeyListener true
+                }
+                else {
                     when (keyEvent.keyCode) {
                         KeyEvent.KEYCODE_DPAD_LEFT -> _viewModel.goPrev()
                         KeyEvent.KEYCODE_DPAD_RIGHT -> _viewModel.goNext()
@@ -76,15 +90,7 @@ class PhotoFragment() : Fragment(R.layout.fragment_photo) {
                     }
                     return@setOnKeyListener true
                 }
-                else{
-                    when (keyEvent.keyCode) {
-                        KeyEvent.KEYCODE_DPAD_DOWN -> _videoView.showController()
-                        KeyEvent.KEYCODE_DPAD_LEFT -> _viewModel.goPrev()
-                        KeyEvent.KEYCODE_DPAD_RIGHT -> _viewModel.goNext()
-                        else -> return@setOnKeyListener false
-                    }
-                    return@setOnKeyListener true
-                }
+
             }
             return@setOnKeyListener false
         }
@@ -92,36 +98,27 @@ class PhotoFragment() : Fragment(R.layout.fragment_photo) {
         dummyButton.setOnFocusChangeListener { view, b ->
             Log.d("PhotoFragment", "dummyButton focus " + b.toString())
             if(b) {
-                if(_videoView.visibility == View.VISIBLE){
-                    _videoView.hideController()
+                if(_currentMediaView is StyledPlayerView){
+                    (_currentMediaView as StyledPlayerView).hideController()
                 }
             }
         }
-
-        _viewModel.currentPhoto.onEach{
-            if(it != null) {
-                _videoView.visibility = View.INVISIBLE
-                _showImageView.visibility = View.VISIBLE
-                _hideImageView.visibility = View.VISIBLE
-                changeImage(it)
-            }
-        }.launchIn(viewLifecycleOwner.lifecycleScope)
-
-        _viewModel.currentVideoRequest.onEach{ it ->
-            if(it != null) {
-                _videoView.visibility = View.VISIBLE
-                _videoView.requestFocus()
-                _showImageView.visibility = View.INVISIBLE
-                _hideImageView.visibility = View.INVISIBLE
-                Log.d("mediaItem", it.url)
-                val mediaItem = MediaItem.fromUri(it.url)
-                _player?.let{
-                    it.setMediaItem(mediaItem)
-                    it.playWhenReady = true
-                    it.prepare()
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    _viewModel.currentMedia.collect {
+                        changeMedia(it)
+                    }
+                }
+                launch {
+                    _viewModel.errorMessageId.collect {
+                        it?.let { messageId ->
+                            Toast.makeText(context, getText(messageId), Toast.LENGTH_LONG).show()
+                        }
+                    }
                 }
             }
-        }.launchIn(viewLifecycleOwner.lifecycleScope)
+        }
     }
 
     override fun onStart() {
@@ -137,61 +134,91 @@ class PhotoFragment() : Fragment(R.layout.fragment_photo) {
     }
 
     private fun initializeVideoPlayer() {
-        //Minimum Video you want to buffer while Playing
-        val MIN_BUFFER_DURATION = 2000
-        //Max Video you want to buffer during PlayBack
-        val MAX_BUFFER_DURATION = 10000
-        //Min Video you want to buffer before start Playing it
-        val MIN_PLAYBACK_START_BUFFER = 100
-        //Min video You want to buffer when user resumes video
-        val MIN_PLAYBACK_RESUME_BUFFER = 2000
+        val MIN_BUFFER_DURATION = 2000      //Minimum Video you want to buffer while Playing
+        val MAX_BUFFER_DURATION = 10000     //Max Video you want to buffer during PlayBack
+        val MIN_PLAYBACK_START_BUFFER = 100 //Min Video you want to buffer before start Playing it
+        val MIN_PLAYBACK_RESUME_BUFFER = 2000   //Min video You want to buffer when user resumes video
 
-        val loadControl: LoadControl = DefaultLoadControl.Builder()
-            .setAllocator(DefaultAllocator(true, 16))
-            .setBufferDurationsMs(
-                MIN_BUFFER_DURATION,
-                MAX_BUFFER_DURATION,
-                MIN_PLAYBACK_START_BUFFER,
-                MIN_PLAYBACK_RESUME_BUFFER
-            )
-            .setTargetBufferBytes(-1)
-            .setPrioritizeTimeOverSizeThresholds(true).createDefaultLoadControl()
+        for(videoView in _videoViews) {
+            val loadControl: LoadControl = DefaultLoadControl.Builder()
+                .setAllocator(DefaultAllocator(true, 16))
+                .setBufferDurationsMs(
+                    MIN_BUFFER_DURATION,
+                    MAX_BUFFER_DURATION,
+                    MIN_PLAYBACK_START_BUFFER,
+                    MIN_PLAYBACK_RESUME_BUFFER
+                )
+                .setTargetBufferBytes(-1)
+                .setPrioritizeTimeOverSizeThresholds(true).createDefaultLoadControl()
 
-        val trackSelector: TrackSelector = DefaultTrackSelector(this.context!!)
+            val trackSelector: TrackSelector = DefaultTrackSelector(this.context!!)
 
-        _player = ExoPlayer.Builder(this.context!!)
-            .setTrackSelector(trackSelector)
-            .setLoadControl(loadControl)
-            .build()
-            .also { exoPlayer ->
-                _videoView.videoView.player = exoPlayer
-            }
+            val player = ExoPlayer.Builder(this.context!!)
+                .setTrackSelector(trackSelector)
+                .setLoadControl(loadControl)
+                .build()
+                .also { exoPlayer ->
+                    videoView.player = exoPlayer
+                }
+            _videoPlayers.add(player)
+        }
+        _viewModel.setVidePlayers(_videoPlayers)
     }
 
     private fun releaseVidePlayer() {
-        _player?.release()
-        _player = null
+        for(player in _videoPlayers) {
+            player.release()
+        }
+        _videoPlayers.clear()
     }
 
-    private fun changeImage(bmp: Bitmap){
-        val fadeDuration = if(_slideshowMode) 1500L else 0L // msec
+    private fun changeMedia(media: PhotoViewModel.Media){
+        Log.d("changeMedia", media.toString())
 
-        _hideImageView.setImageBitmap(bmp)
-        ObjectAnimator.ofFloat(_hideImageView, "alpha", 1.0f).apply {
+        val fadeDuration = if(_slideshowMode) 1500L else 100 // msec
+        var nextView: View? = null
+
+        if(media.bitmap != null){ // next media is bitmap
+            for(imageView in _imageViews){
+                if(imageView != _currentMediaView){
+                    imageView.setImageBitmap(media.bitmap!!)
+                    nextView = imageView
+                }
+            }
+        }
+        else{ // next media is video
+            for(videoView in _videoViews){
+                if(videoView.player == media.videoPlayer){
+                    nextView = videoView
+                }
+            }
+        }
+        if(nextView == null){ // next media is blank
+            nextView = _imageViews[0]
+            nextView.setImageResource(R.drawable.blank_image)
+        }
+
+        for(videoView in _videoViews){
+            videoView.focusable = if(videoView.player == media.videoPlayer) View.FOCUSABLE else View.NOT_FOCUSABLE
+        }
+
+        nextView.alpha = 0.0f
+        nextView.visibility = View.VISIBLE
+
+        ObjectAnimator.ofFloat(nextView, "alpha", 1.0f).apply {
             duration = fadeDuration
             start()
         }
-        ObjectAnimator.ofFloat(_showImageView, "alpha", 0.0f).apply {
+        ObjectAnimator.ofFloat(_currentMediaView, "alpha", 0.0f).apply {
             duration = fadeDuration
             start()
         }
 
         _viewModel.currentPhotoMetadata?.let {
-            photoInfo.text = it.metadataRemote.timestamp.format(DateTimeFormatter.ISO_LOCAL_DATE)
+            photoInfo.text =
+                it.metadataRemote.timestamp.format(DateTimeFormatter.ISO_LOCAL_DATE)
         }
 
-        val tmp = _hideImageView
-        _hideImageView = _showImageView
-        _showImageView = tmp
+        _currentMediaView = nextView
     }
 }
--- app/src/main/java/com/satohk/gphotoframe/viewmodel/PhotoViewModel.kt
+++ app/src/main/java/com/satohk/gphotoframe/viewmodel/PhotoViewModel.kt
@@ -1,44 +1,53 @@
 package com.satohk.gphotoframe.viewmodel
 
 import android.graphics.Bitmap
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.satohk.gphotoframe.domain.*
-import com.satohk.gphotoframe.repository.remoterepository.CachedPhotoRepository
+import com.satohk.gphotoframe.domain.AccountState
+import com.satohk.gphotoframe.domain.FilteredPhotoList
+import com.satohk.gphotoframe.domain.PhotoSelector
 import com.satohk.gphotoframe.repository.data.PhotoMetadata
+import com.satohk.gphotoframe.repository.remoterepository.CachedPhotoRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.upstream.HttpDataSource
+import com.satohk.gphotoframe.R
+import kotlinx.coroutines.flow.*
 
 
 class PhotoViewModel(
     private val _accountState: AccountState
-) : ViewModel() {
+) : ViewModel(), Player.Listener {
 
-    data class VideoRequest(
-        val header: Map<String, String>,
-        val url: String
+    data class Media (
+        val bitmap: Bitmap?,
+        val videoPlayer: ExoPlayer?
     )
 
-    private val _currentPhoto = MutableStateFlow<Bitmap?>(null)
-    val currentPhoto: StateFlow<Bitmap?> get() = _currentPhoto
-    private val _currentVideoRequest = MutableStateFlow<VideoRequest?>(null)
-    val currentVideoRequest: StateFlow<VideoRequest?> get() = _currentVideoRequest
+    private lateinit var _videoPlayers: List<ExoPlayer>
+    private val _currentMedia = MutableStateFlow(Media(null, null))
+    val currentMedia: StateFlow<Media> get() = _currentMedia
+    private var _currentPlayer: ExoPlayer? = null
     var photoWidth: Int = 1024
     var photoHeight: Int = 768
     var currentPhotoMetadata: PhotoMetadata? = null
         private set
 
-    private var _gridContents: GridContents = GridContents()
+    private var _gridContents: GridContents? = null
 
     private var _filteredPhotoList: FilteredPhotoList? = null
     private var _photoSelector: PhotoSelector? = null
     private var _slideShow: Boolean = true
     private var _showIndex: Int = 0
+
+    private val _errorMessageId = MutableSharedFlow<Int>()
+    val errorMessageId: SharedFlow<Int?> get() = _errorMessageId
 
     init{
         _accountState.photoRepository.onEach {
@@ -48,19 +57,36 @@ class PhotoViewModel(
         }.launchIn(viewModelScope)
     }
 
+    fun setVidePlayers(videoPlayers: List<ExoPlayer>){
+        _videoPlayers = videoPlayers
+        for(player in videoPlayers){
+            player.addListener(this)
+        }
+    }
+
+    fun getAvailablePlayer(): ExoPlayer {
+        for(player in _videoPlayers){
+            if(_currentMedia.value.videoPlayer != player){
+                return player
+            }
+        }
+        check(false) { "This code won't run" }
+        return _videoPlayers[0]  // "This code won't run"
+    }
+
     fun onStart(gridContents: GridContents?, slideShow: Boolean, showIndex: Int){
         if(_accountState.photoRepository.value != null){
             if (gridContents != null) {
-                _gridContents = gridContents
+                this._gridContents = gridContents
             }
             else{
-                _gridContents = GridContents(
+                this._gridContents = GridContents(
                     _accountState.settingRepository.setting.value.screensaverSearchQuery
                 )
             }
             _slideShow = slideShow
             _showIndex = showIndex
-            initPhotoSelector(_accountState.photoRepository.value!!, _gridContents, _slideShow, _showIndex)
+            initPhotoSelector(_accountState.photoRepository.value!!, this._gridContents!!, _slideShow, _showIndex)
         }
     }
 
@@ -98,9 +124,10 @@ class PhotoViewModel(
             showIndex
         )
 
-        _photoSelector!!.currentPhotoMetadata.onEach {
+        _photoSelector!!.currentPhotoMetadata.onEach { it ->
             it?.let{ metadata: PhotoMetadata ->
                 currentPhotoMetadata = metadata
+                Log.d("currentPhotoMetadata", currentPhotoMetadata.toString())
                 if(metadata.metadataRemote.mimeType.startsWith("image")) {
                     withContext(Dispatchers.IO) {
                         val bmp = _accountState.photoRepository.value!!.getPhotoBitmap(
@@ -109,21 +136,47 @@ class PhotoViewModel(
                             photoHeight,
                             false
                         )
-                        _currentPhoto.value = bmp
+                        _currentMedia.value = Media(bmp, null)
                     }
                 }
                 else if(metadata.metadataRemote.mimeType.startsWith("video")){
                     val tmp = _accountState.photoRepository.value!!.getMediaAccessHeaderAndUrl(metadata.metadataRemote)
-                    _currentVideoRequest.value = VideoRequest(
-                        tmp.first,
-                        tmp.second
-                    )
+                    val mediaUrl = tmp.second
+                    val mediaItem = MediaItem.fromUri("https://www.google.com/aaa")//mediaUrl)
+                    _currentPlayer = getAvailablePlayer()
+                    _currentPlayer?.let { player ->
+                        player.setMediaItem(mediaItem)
+                        player.playWhenReady = true
+                        player.prepare()
+                        if(!this._slideShow){ // Change media immediately if in manual mode
+                            _currentMedia.value = Media(null, _currentPlayer)
+                        }
+                    }
                 }
             }
         }.launchIn(viewModelScope)
 
         viewModelScope.launch {
             _photoSelector!!.start(slideShow)
+        }
+    }
+
+    //Player events
+    override fun onPlayerError(error: PlaybackException) {
+        Log.d("onPlayerError", "${error.toString()}, ${error.cause.toString()}")
+        viewModelScope.launch {
+            _errorMessageId.emit(
+                R.string.msg_video_load_error
+            )
+        }
+    }
+
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        Log.d("onPlayerStateChanged", "playWhenReady=${playWhenReady}, playbackState=${playbackState}")
+        if(playbackState == ExoPlayer.STATE_READY){
+            if(this._slideShow) { // Change media when ready if in slideshow mode
+                _currentMedia.value = Media(null, _currentPlayer)
+            }
         }
     }
 }
